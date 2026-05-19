670e718f964d6fa027e5f97cae16b07c19f42d08
Abel Sanchez <tu_correo_github@example.com>
2026-05-19 16:29:32 +0200

    Actualización del mensaje

--- index.php
+++ index.php
@@ -10,6 +10,6 @@ if ($conexion->connect_error) {
     die("Error de conexion: " . $conexion->connect_error);
 }
 
-echo "Conexion exitosa a MariaDB";
+echo "Conexion exitosa a MariaDB - Abel Sánchez Ramos";
 
 ?>
